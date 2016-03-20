4e812e2c5a3fd4433548eddc31e0e76baaedc790
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2016-03-20 18:12:39 +0100

    add form button to Project form

--- src/components/ProjectForm/index.js
+++ src/components/ProjectForm/index.js
@@ -1,13 +1,8 @@
 import React, {Component, PropTypes} from 'react';
 import ui from 'redux-ui';
 import objectAssign from 'object-assign';
-import {TextField, IconButton, SelectField, MenuItem} from 'material-ui/lib';
+import {TextField, IconButton, SelectField, MenuItem, RaisedButton} from 'material-ui/lib';
 import CheckCircle from 'material-ui/lib/svg-icons/action/done';
-
-//This is a stateless functional component. (Also known as pure or dumb component)
-//More info: https://facebook.github.io/react/blog/2015/10/07/react-v0.14.html#stateless-functional-components
-//And https://medium.com/@joshblack/stateless-components-in-react-0-14-f9798f8b992d
-//Props are being destructured below to extract the savings object to shorten calls within component.
 
 class ProjectForm extends Component {
 
@@ -90,6 +85,10 @@ class ProjectForm extends Component {
           >
           {projectOptions}
         </SelectField>
+        <RaisedButton
+          label="Save"
+          onClick={this.handleSubmit.bind(this)}
+          primary={true} />
       </form>
     );
   }
