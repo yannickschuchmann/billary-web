f1267cc53fb5ebad079401e4251714f5f3401dc0
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2016-03-21 19:48:50 +0100

    enhance visual day

--- src/components/VisualDay/_item.js
+++ src/components/VisualDay/_item.js
@@ -1,20 +1,55 @@
 import React, {Component, PropTypes} from 'react';
 import ui from 'redux-ui';
-
+import objectAssign from 'object-assign';
+import {moment} from '../../businessLogic/calendarHelper';
+import {hourScale} from './index';
 class VisualDayItem extends Component {
   static propTypes = {
     index: PropTypes.number,
-    item: PropTypes.object
+    item: PropTypes.object,
+    ui: PropTypes.object,
+    updateUI: PropTypes.func
+  };
+
+  static minuteInPx = hourScale / 60;
+
+  getX() {
+    const startedAt = this.props.item.started_at_overhang.clone();
+    return startedAt.diff(startedAt
+                            .clone()
+                            .startOf('day'), 'minute')
+                            * VisualDayItem.minuteInPx;
+  };
+
+  getWidth() {
+    const item = this.props.item;
+    return Math.abs(item.stopped_at_overhang
+                      .clone()
+                      .diff(item.started_at_overhang, 'minute')) * VisualDayItem.minuteInPx;
+  };
+
+  componentWillMount() {
+    this.props.updateUI("itemStyle", objectAssign({}, this.props.ui.itemStyle, {
+      transform: `translateX(${this.getX()}px)`,
+      width: `${this.getWidth()}px`
+    }));
   };
 
   render() {
     return (
-      <div className="visual-day-item">
-        {this.props.item.duration}
+      <div className="visual-day-item" style={this.props.ui.itemStyle}>
+        {this.props.item.projectNames.join(" - ")}
       </div>
     );
   };
 }
 
 
-export default ui({state: {}})(VisualDayItem);
+export default ui({
+  state: {
+    itemStyle: {
+      transform: "translateX(0)",
+      width: 0
+    }
+  }
+})(VisualDayItem);
--- src/components/VisualDay/index.js
+++ src/components/VisualDay/index.js
@@ -2,6 +2,10 @@ import React, {Component, PropTypes} from 'react';
 import ui from 'redux-ui';
 import objectAssign from 'object-assign';
 import {moment} from '../../businessLogic/calendarHelper';
+import DragHandle from 'material-ui/lib/svg-icons/editor/drag-handle';
+
+const hourScale = 100;
+const spacingAround = 40; //px
 
 class VisualDay extends Component {
   static propTypes = {
@@ -9,17 +13,33 @@ class VisualDay extends Component {
     selectedDay: PropTypes.object
   };
 
-  static hourScale = 100; //px
-  static spacingAround = 20; //px
 
   render() {
     let grid = [];
     for(let i = 0; i < 24; i++) {
-      grid.push(<div className="grid-line">{i}</div>);
+      grid.push(
+        <div className="grid-line" style={{width: hourScale + "px"}}>
+          <div className="label">{moment().hour(i).minute(0).second(0).format("H:mm")}</div>
+        </div>
+      );
     }
+    grid.push(
+      <div className="grid-line last">
+        <div className="label">{moment().hour(24).minute(0).second(0).format("H:mm")}</div>
+      </div>
+    )
 
     return (
       <div className="visual-day-wrap">
+        <div className="visual-day-scrollbar">
+          <div className="visual-day-thumb"><DragHandle style={{
+              transform: "rotateZ(90deg)",
+              height: 13,
+              width: 13,
+              opacity: 0.21,
+              fill: "#fff"
+            }}/></div>
+        </div>
         <div className="visual-day-header" style={{
             padding: this.props.ui.style.padding
           }}>
@@ -29,10 +49,12 @@ class VisualDay extends Component {
           <div className="visual-day-scroll">
             <div className="visual-day" ref="container" style={this.props.ui.style}>
               <div className="grid" style={{
-                  left: `${VisualDay.spacingAround}px`,
-                  right: `${VisualDay.spacingAround}px`
+                  left: `${spacingAround}px`,
+                  right: `${spacingAround}px`
                 }}>{grid}</div>
-              {this.props.children}
+              <div className="visual-day-rows">
+                {this.props.children}
+              </div>
             </div>
           </div>
         </div>
@@ -41,12 +63,12 @@ class VisualDay extends Component {
   };
 }
 
-
+export {hourScale, spacingAround}
 export default ui({
   state: {
     style: {
-      width: (24 * VisualDay.hourScale) + "px",
-      padding: `0 ${VisualDay.spacingAround}px`
+      width: (24 * hourScale) + "px",
+      padding: `0 ${spacingAround}px`
     }
   }
 })(VisualDay);
