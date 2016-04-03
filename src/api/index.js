30aaff210996e42e9df6715c9e2039bfc324d3ad
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2016-04-03 13:37:42 +0200

    Add credentials to API requests

--- src/api/index.js
+++ src/api/index.js
@@ -4,85 +4,77 @@ const superagentPromisePlugin = require('superagent-promise-plugin');
 
 export const API_PATH = "http://api.localhost:3000/v1";
 
-export function getProjects() {
-  return request
-          .get(API_PATH + "/projects")
+
+const getAuthCookie = () => {
+  const b = localStorage.getItem("authHeaders");
+  return b ? JSON.parse(b) : {};
+};
+
+const authorizedReq = (req) => {
+  const authHeaders = getAuthCookie();
+  return req
+          .withCredentials()
+          .set('access-token', authHeaders["access-token"])
+          .set('expiry', authHeaders["expiry"])
+          .set('token-type', authHeaders["token-type"])
+          .set('uid', authHeaders["uid"])
+          .set('client', authHeaders["client"])
           .use(superagentPromisePlugin)
           .end();
+};
 
+export function getProjects() {
+  return authorizedReq(request.get(API_PATH + "/projects"));
 }
 
 export function patchProject(project) {
-  return request
-          .patch(API_PATH + "/projects/" + project.id)
-          .send({project})
-          .use(superagentPromisePlugin)
-          .end();
-
+  return authorizedReq(request
+                        .patch(API_PATH + "/projects/" + project.id)
+                        .send({project})
+                      );
 }
 
 export function postProject(project) {
-  return request
-          .post(API_PATH + "/projects")
-          .send({project})
-          .use(superagentPromisePlugin)
-          .end();
-
+  return authorizedReq(request
+                        .post(API_PATH + "/projects")
+                        .send({project})
+                      );
 }
 
 export function deleteProject(projectId) {
-  return request
-          .delete(API_PATH + "/projects/" + projectId)
-          .use(superagentPromisePlugin)
-          .end();
-
+  return authorizedReq(request.delete(API_PATH + "/projects/" + projectId));
 }
 
 export function getTimeEntries() {
-  return request
-          .get(API_PATH + "/time_entries")
-          .use(superagentPromisePlugin)
-          .end();
+  return authorizedReq(request.get(API_PATH + "/time_entries"));
 }
 
 export function getCurrentTimeEntry() {
-  return request
-          .get(API_PATH + "/time_entries/current")
-          .use(superagentPromisePlugin)
-          .end();
+  return authorizedReq(request.get(API_PATH + "/time_entries/current"));
 }
 
 export function postTimeEntry(entry) {
-  return request
-          .post(API_PATH + "/time_entries")
-          .send({
-            time_entry: entry
-          })
-          .use(superagentPromisePlugin)
-          .end();
+  return authorizedReq(request
+                        .post(API_PATH + "/time_entries")
+                        .send({
+                          time_entry: entry
+                        })
+                      );
 }
 
 export function patchTimeEntry(entry) {
-  return request
-          .patch(API_PATH + "/time_entries/" + entry.id)
-          .send({
-            time_entry: entry
-          })
-          .use(superagentPromisePlugin)
-          .end();
+  return authorizedReq(request
+                        .patch(API_PATH + "/time_entries/" + entry.id)
+                        .send({
+                          time_entry: entry
+                        })
+                      );
 }
 
 export function deleteTimeEntry(entryId) {
-  return request
-          .delete(API_PATH + "/time_entries/" + entryId)
-          .use(superagentPromisePlugin)
-          .end();
-
+  return authorizedReq(request.delete(API_PATH + "/time_entries/" + entryId));
 }
 
 export function stopTimeEntry() {
-  return request
-          .post(API_PATH + "/time_entries/stop")
-          .use(superagentPromisePlugin)
-          .end();
+  return authorizedReq(request.post(API_PATH + "/time_entries/stop"));
 }
