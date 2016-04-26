ea60fac063663cd2b11770b0258d7addf1efe021
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2016-04-26 12:28:05 +0200

    Add direct link to pdf to invoicesListing

--- src/components/InvoiceListing/index.js
+++ src/components/InvoiceListing/index.js
@@ -6,25 +6,30 @@ import TableRow from 'material-ui/lib/table/table-row';
 import TableHeader from 'material-ui/lib/table/table-header';
 import TableRowColumn from 'material-ui/lib/table/table-row-column';
 import TableBody from 'material-ui/lib/table/table-body';
+import Pdf from 'material-ui/lib/svg-icons/image/picture-as-pdf';
+import { Link } from 'react-router';
+import { API_PATH } from '../../api/helpers';
 
 const InvoiceListing = ({invoices, onSelect, selected}) => {
   const items = invoices.map((item, i) => (
-    <TableRow key={i} selected={i === selected}>
+    <TableRow key={i}>
       <TableRowColumn>{item.number}</TableRowColumn>
       <TableRowColumn>{item.client.name}</TableRowColumn>
       <TableRowColumn>{item.price}</TableRowColumn>
       <TableRowColumn>{moment(item.created_at).format("L")}</TableRowColumn>
+      <TableRowColumn><a target="blank" href={`${API_PATH}/invoices/${item.id}.pdf`}><Pdf/></a></TableRowColumn>
     </TableRow>
   ));
 
   return (
-    <Table multiSelectable={false} className="invoice-listing">
+    <Table multiSelectable={false} className="invoice-listing" selectable={false}>
       <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
         <TableRow>
           <TableHeaderColumn tooltip="Invoice number">#</TableHeaderColumn>
           <TableHeaderColumn>Client</TableHeaderColumn>
           <TableHeaderColumn>Price (incl. VAT)</TableHeaderColumn>
           <TableHeaderColumn>Created At</TableHeaderColumn>
+          <TableHeaderColumn></TableHeaderColumn>
         </TableRow>
       </TableHeader>
       <TableBody
