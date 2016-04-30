5c71c2b1b6becacb0161d8d7b37e4ffda1aa2bb0
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2016-04-30 23:38:51 +0200

    Basic asset caching

--- src/index.js
+++ src/index.js
@@ -23,3 +23,16 @@ store.dispatch(configure()).then(() => {
     </Provider>, document.getElementById('app-container')
   );
 });
+
+//make sure that Service Workers are supported.
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/service-worker.js', {scope: '/'})
+    .then(function (registration) {
+        console.log(registration);
+    })
+    .catch(function (e) {
+        console.error(e);
+    })
+} else {
+    console.log('Service Worker is not supported in this browser.');
+}
